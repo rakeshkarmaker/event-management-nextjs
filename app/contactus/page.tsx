e702d9add24c9ea03e3722f9722a00c1953e023c
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2025-10-31 12:18:40 +0600

    v1.2.0- Implemented mongoose DB

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -1,16 +1,9 @@
-import Form from 'next/form'
+import ContactForm from '@/components/ContactForm'
 
 const Contactus = () => {
   return (
     <div>
-        <h1>Contact Us</h1> 
-        <p>If you have any questions, feel free to reach out to us.</p>
-        
-        <Form action="/search">
-      <input name="query" />
-      <button type="submit">Submit</button>
-    </Form>
-
+        <ContactForm />
     </div>
 
   )
