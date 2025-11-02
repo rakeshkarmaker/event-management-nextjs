7d21d4dd0133e64523211427e6b30bf5dce55ad4
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2025-11-02 12:14:23 +0600

    v1.4.0- Stable final version

---
+++ app/create-event/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreateEvent = () => {
+    // so we will create a new event here!
+
+  return (
+    <div className="create-event">
+        <h1>Create a New Event</h1>
+    </div>
+  )
+}
+
+export default CreateEvent
